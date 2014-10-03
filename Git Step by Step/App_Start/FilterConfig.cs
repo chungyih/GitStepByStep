b2cc0962af2a08702c1747adb9a8ae3c02c4933e
chungyih <wii.at.git@gmail.com>
2014-10-03 15:13:29 +0800

    initial

---
+++ Git Step by Step/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Git_Step_by_Step
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Git Step by Step/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Git_Step_by_Step.MvcApplication" Language="C#" %>
